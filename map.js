6d4e260b0bb071ec4e59ff28020156f4ebbbf3f7
Hassan-Almahdawi <111483725+Hassan-Almahdawi@users.noreply.github.com>
2022-09-25 20:30:33 +1000

    Add files via upload

---
+++ map.js
@@ -0,0 +1,5 @@
+var Mymap = "bushfires.vg.json";
+vegaEmbed("#bushfires", Mymap);
+// .then(function(result) {
+// // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
