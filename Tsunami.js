3e379cef30e38e0fa7fb4a688bc98efdb1a48e4b
Hassan-Almahdawi <111483725+Hassan-Almahdawi@users.noreply.github.com>
2022-10-10 19:27:13 +1100

    Add files via upload

---
+++ Tsunami.js
@@ -0,0 +1,5 @@
+var Myvis = "Tsunami.vg.json";
+vegaEmbed("#Tsunami", Myvis);
+// .then(function(result) {
+// // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
